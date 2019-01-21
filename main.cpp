0e7c12e9a5c036462cd001b41e3bf56e7997eb2c
Fabio Roncato <fabio_xyz@hotmail.com>
2019-01-21 22:25:46 +0100

    updated to a good point

--- main.cpp
+++ main.cpp
@@ -19,9 +19,9 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    const int images_in_x = 57;
-    const int images_in_y =19;
-    const int border_pixel = 400;
+    const int images_in_x = 26; //23*47 = 1081 // 26*42 = 1092
+    const int images_in_y = 42;
+    const int border_pixel = 1;
     const int imageDimension = 401;
     QDate startingDay(2010, 5, 17);
     QDate endingDay(2013, 5, 4);
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
 //    cout << "Days between date: " << daysBetweenDate << endl;
 
     // open file with information of date, injection yes or no, point of injection (if done)
-    QFile file("C:\\Users\\Fabio Roncato\\Documents\\images_rebif\\rebif\\date.txt");
+    QFile file("C:\\Users\\Fabio Roncato\\Documents\\Qt\\09_01_2019_rebif\\date.txt");
     if(!file.open(QFile::ReadOnly|QFile::Text))
         cout << "No file found" << endl;
 
@@ -71,7 +71,7 @@ int main(int argc, char *argv[])
       data.append(dateString);
       iniezione.append(yesOrNo);
       posizione.append(position);
-      cout << dateString.toStdString() << " - " << yesOrNo.toStdString() << " - " << position.toStdString() << endl;
+     // cout << dateString.toStdString() << " - " << yesOrNo.toStdString() << " - " << position.toStdString() << endl;
     }
     cout << endl;
     cout << "data elements: " << data.count() << endl;
@@ -91,8 +91,8 @@ int main(int argc, char *argv[])
 
 
     int index_x=0, index_y=0;
-    Mat big_image_new(2*border_pixel+(images_in_y*imageDimension)+(images_in_y-1)*1, 2*border_pixel+(images_in_x*imageDimension)+(images_in_x-1)*1, CV_8UC3, Scalar(255,255,255));
-
+    Mat big_image_new(2*border_pixel+(images_in_y*imageDimension)+(images_in_y-1)*1, 2*border_pixel+(images_in_x*imageDimension)+(images_in_x-1)*1, CV_8UC3, Scalar(0,0,0));
+    Mat big_image_new_gray, big_image_new_gray_inverse;
 
     int circleRadious = imageDimension/6;
     // create an image
@@ -112,12 +112,12 @@ int main(int argc, char *argv[])
             position="0";
             filenameCurrentImageBody = "C:\\Users\\Fabio Roncato\\Documents\\images_rebif\\rebif\\ridimension\\injectionSite_0.png";
         }
-        cout << "path: " << filenameCurrentImageBody.toStdString() << endl;
+        //cout << "path: " << filenameCurrentImageBody.toStdString() << endl;
         Mat currentImageBody = imread(filenameCurrentImageBody.toStdString(),CV_LOAD_IMAGE_COLOR);
         if(position.toInt() != 0){
-            putText(currentImageBody, dayDate.toStdString(), Point(imageDimension/4,imageDimension-60), FONT_HERSHEY_COMPLEX_SMALL, 1.4, CV_RGB(0,0,0), 0.9 );
+            putText(currentImageBody, dayDate.toStdString(), Point(imageDimension/12,imageDimension-60), FONT_HERSHEY_TRIPLEX , 1.7, CV_RGB(0,0,0), 1.0 );
             // draw circle in center
-            circle(currentImageBody, Point((imageDimension-1)/2, (imageDimension-1)/2), circleRadious, CV_RGB(0,0,0), 2 , 8, 0);
+            circle(currentImageBody, Point((imageDimension-1)/2, (imageDimension-1)/2), circleRadious, CV_RGB(0,0,0), -2 , 8, 0);
         }
 
         currentImageBody.copyTo(big_image_new(cv::Rect(border_pixel+index_x*(imageDimension+1),border_pixel+index_y*(imageDimension+1),imageDimension, imageDimension)));
@@ -130,10 +130,21 @@ int main(int argc, char *argv[])
             index_x=0;
         }
     }
-    imwrite("C:\\Users\\Fabio Roncato\\Documents\\Qt\\09_01_2019_rebif\\rebif.jpg" , big_image_new );
 
+    cv::cvtColor(big_image_new, big_image_new_gray, CV_BGR2GRAY);
+    big_image_new_gray_inverse = 255 - big_image_new_gray;
+    cv::threshold(big_image_new_gray_inverse,big_image_new_gray_inverse,1,255,cv::THRESH_BINARY);
+    big_image_new_gray = 255-big_image_new_gray_inverse;
 
+    imwrite("C:\\Users\\Fabio Roncato\\Documents\\Qt\\09_01_2019_rebif\\rebif_color.jpg" , big_image_new );
+    imwrite("C:\\Users\\Fabio Roncato\\Documents\\Qt\\09_01_2019_rebif\\rebif_gray.jpg" , big_image_new_gray );
+    imwrite("C:\\Users\\Fabio Roncato\\Documents\\Qt\\09_01_2019_rebif\\rebif_gray_inverse.jpg" , big_image_new_gray_inverse );
 
+    QString final_image = "C:\\Users\\Fabio Roncato\\Documents\\Qt\\09_01_2019_rebif\\test_image.jpg";
+    Mat test_image = imread(final_image.toStdString(),CV_LOAD_IMAGE_COLOR);
+    Mat outputMat;
+    test_image.copyTo(outputMat, big_image_new_gray);
+    imwrite("C:\\Users\\Fabio Roncato\\Documents\\Qt\\09_01_2019_rebif\\outputMat.jpg" , outputMat );
 
 
     Size size(image.cols/2, image.rows/2);
