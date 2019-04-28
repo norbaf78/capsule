6b7d28774bc2c6a2a8c7ed9f4774a6df2e9a524c
Fabio Roncato <fabio_xyz@hotmail.com>
2019-04-28 16:23:12 +0200

    comment update

--- main.cpp
+++ main.cpp
@@ -23,31 +23,34 @@ int main(int argc, char *argv[])
     const int images_in_x = 26; //23*47 = 1081 // 26*42 = 1092
     const int images_in_y = 42;
     const int border_pixel = 1;
-    const int imageDimension = 401;
+    const int imageDimension = 401; // dimension images in "C:\Users\Fabio Roncato\Documents\images_rebif\rebif\ridimension" imageDimension x imageDimension
+    const int circleRadious = imageDimension/6;
     QDate startingDay(2010, 5, 17);
     QDate endingDay(2013, 5, 4);
 
 
     ////////////////////////////////////////////////////////////////////////////////////
-    // open file with information of date, injection yes or no, point of injection (if done)
+    // open file with information of date, injection yes or no, point of injection (if injection has been done)
     ////////////////////////////////////////////////////////////////////////////////////
     QFile file("C:\\Users\\Fabio Roncato\\Documents\\Qt\\09_01_2019_rebif\\date.txt");
     if(!file.open(QFile::ReadOnly|QFile::Text))
         cout << "No file found" << endl;
 
-    // get the data from the files and create three QStringList with those informations
+    ////////////////////////////////////////////////////////////////////////////////////
+    // read the data from the file and create three QStringList with those informations
+    ////////////////////////////////////////////////////////////////////////////////////
     QStringList data;
     QStringList iniezione;
     QStringList posizione;
     while ( !file.atEnd() ) {
-      QString line = file.readLine();
-      QStringList list = line.split(" ");
-      QString dateString = list[0];
-      QString yesOrNo = list[1];
-      QString position = list[2];
-      data.append(dateString);
-      iniezione.append(yesOrNo);
-      posizione.append(position);
+      QString line = file.readLine();   // read the line
+      QStringList list = line.split(" "); // split the line
+      QString dateString = list[0]; // read the date
+      QString yesOrNo = list[1]; // read yes or no (if the injection has been done or not)
+      QString position = list[2]; // read the position (in case the injection has not been done the value will been 0)
+      data.append(dateString); // the value are append for the data in "data"
+      iniezione.append(yesOrNo); // the value are append for the injection in "iniezione"
+      posizione.append(position); // the value are append for the injection position in "posizione"
      // cout << dateString.toStdString() << " - " << yesOrNo.toStdString() << " - " << position.toStdString() << endl;
     }
     cout << endl;
@@ -66,13 +69,13 @@ int main(int argc, char *argv[])
 
 
     ////////////////////////////////////////////////////////////////////////////////////
-    /// create an image containing a number of days images (imageDimension is the dimenion of the images)
+    /// create an image containing all the images for all the days (imageDimension is the dimenion of the small images used)
     ///////////////////////////////////////////////////////////////////////////////////////
     int index_x=0, index_y=0;
     Mat big_image_new(2*border_pixel+(images_in_y*imageDimension)+(images_in_y-1)*1, 2*border_pixel+(images_in_x*imageDimension)+(images_in_x-1)*1, CV_8UC3, Scalar(0,0,0));
 
     ////////////////////////////////////////////////////////////////////////////////////
-    /// load the original image
+    /// load the original image will be used ad background
     ///////////////////////////////////////////////////////////////////////////////////////
     QString final_image = "C:\\Users\\Fabio Roncato\\Documents\\images_rebif\\test_image.jpg";
     Mat test_image = imread(final_image.toStdString(),CV_LOAD_IMAGE_COLOR);
@@ -80,12 +83,11 @@ int main(int argc, char *argv[])
     cv::cvtColor(test_image, test_image_gray, CV_BGR2GRAY);
     cv::cvtColor(test_image_gray, test_image, CV_GRAY2BGR);
 
-    int circleRadious = imageDimension/6;
     // create an image
     QString filenameCurrentImageBody;
     QString dayDate;
     QString position;
-    for(int i=0;i< data.count(); i++){
+    for(int i=0;i< data.count(); i++){ // for all the date (for all the days)
         /////////////////////////////////////////////////////////////////////////
         // generating which image(different type depending if injection done and where) take
         /////////////////////////////////////////////////////////////////////////
@@ -93,22 +95,23 @@ int main(int argc, char *argv[])
         if(iniezione[i].compare("yes") == 0){ // iniezione effettuata
             position = posizione[i];
             if(position.toInt() != 0 && position.toInt()< 19) // effettuata frontalmente
-                filenameCurrentImageBody = "C:\\Users\\Fabio Roncato\\Documents\\images_rebif\\rebif\\ridimension2\\injectionSite_front_" + position + ".png";
+                filenameCurrentImageBody = "C:\\Users\\Fabio Roncato\\Documents\\images_rebif\\rebif\\ridimension\\injectionSite_front_" + position + ".png";
             else if(position.toInt() != 0 && position.toInt()>= 19) // effettuata posteriormente
-                filenameCurrentImageBody = "C:\\Users\\Fabio Roncato\\Documents\\images_rebif\\rebif\\ridimension2\\injectionSite_back_" +  position + ".png";
+                filenameCurrentImageBody = "C:\\Users\\Fabio Roncato\\Documents\\images_rebif\\rebif\\ridimension\\injectionSite_back_" +  position + ".png";
         }
         else{ // iniezione non effettuata
             position="0";
-            filenameCurrentImageBody = "C:\\Users\\Fabio Roncato\\Documents\\images_rebif\\rebif\\ridimension2\\injectionSite_0.png";
+            filenameCurrentImageBody = "C:\\Users\\Fabio Roncato\\Documents\\images_rebif\\rebif\\ridimension\\injectionSite_0.png";
         }
         //image has been selected and now circle needle and date will be added
         Mat currentImageBody = imread(filenameCurrentImageBody.toStdString(),CV_LOAD_IMAGE_COLOR);
+        //cv::threshold(currentImageBody,currentImageBody,110,255,cv::THRESH_BINARY);
         if(position.toInt() != 0){
-            putText(currentImageBody, dayDate.toStdString(), Point(imageDimension/12,imageDimension-60), FONT_HERSHEY_TRIPLEX , 1.7, CV_RGB(0,0,0), 2.0 );
+            putText(currentImageBody, dayDate.toStdString(), Point(imageDimension/14,imageDimension-20), FONT_HERSHEY_TRIPLEX , 1.7, CV_RGB(0,0,0), 2.0 );
             circle(currentImageBody, Point((imageDimension-1)/2, (imageDimension-1)/2), circleRadious, CV_RGB(240,240,240), -2 , 8, 0);
         }
 
-
+/*
         /////////////////////////////////////////////////////////////////////////
         // generating an image where the color is the mean color of the same area in the original image
         /////////////////////////////////////////////////////////////////////////
@@ -119,17 +122,21 @@ int main(int argc, char *argv[])
         Scalar mean, stdv;
         cv::meanStdDev(cropped,mean,stdv);
         currentImageVoid.setTo((int)mean[0]);
-
+*/
 
         /////////////////////////////////////////////////////////////////////////
         // generating an cutting image of the same area in the original image
         /////////////////////////////////////////////////////////////////////////
-        roi = cv::Rect(border_pixel+index_x*(imageDimension+1),border_pixel+index_y*(imageDimension+1),imageDimension, imageDimension);
+        Rect roi = cv::Rect(border_pixel+index_x*(imageDimension+1),border_pixel+index_y*(imageDimension+1),imageDimension, imageDimension);
         Mat croppedImage = Mat(test_image_gray, roi);
 
+        Mat MatTransparent;
+        cv::inRange(currentImageBody, cv::Scalar(0,0,0), cv::Scalar(241,241,241), MatTransparent);
+        croppedImage.copyTo(currentImageBody, 255-MatTransparent);
 
+        cv::imshow("transparent pixel", MatTransparent);
+        //imshow( "currentImageVoid", currentImageVoid );
         imshow( "imageBody", currentImageBody );
-        imshow( "currentImageVoid", currentImageVoid );
         imshow( "croppedImage", croppedImage );
         waitKey(0);
 
